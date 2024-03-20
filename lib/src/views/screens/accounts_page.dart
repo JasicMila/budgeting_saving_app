1746c9d008a1aadaa3575213a56f88d92bfa706b
Jasna <jasna.hadzialic@gmail.com>
2024-03-20 13:39:13 +0100

    make sure everything works with account model

--- lib/src/views/screens/accounts_page.dart
+++ lib/src/views/screens/accounts_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart';
-import 'package:budgeting_saving_app/src/models/account.dart';
 
 
 class AccountsPage extends StatefulWidget {
